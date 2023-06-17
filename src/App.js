ad08d56df7a30d8471ff64e207ddc1cc25fe8b01
Adam Kałuża <adam.kaluza.97@gmail.com>
2023-06-17 17:20:56 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink, useNavigate } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Home = () => <h1>Strona główna</h1>;
+const About = () => <h1>O nas</h1>;
+const Services = () => <h1>Usługi</h1>;
+const Contact = () => <h1>Kontakt</h1>;
+const NotFound = () => {
+    return <h1>Nie znaleziono elementu: {window.location.pathname}</h1>;
+};
+
+const App = () => {
+    return (
+        <Router>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink exact to="/" activeClassName="active">
+                            Strona główna
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about" activeClassName="active">
+                            O nas
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/services" activeClassName="active">
+                            Usługi
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact" activeClassName="active">
+                            Kontakt
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact/us" activeClassName="active">
+                            Kontakt - US
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact/pl" activeClassName="active">
+                            Kontakt - PL
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact/de" activeClassName="active">
+                            Kontakt - DE
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/contact/us" element={<Contact />} />
+                <Route path="/contact/pl" element={<Contact />} />
+                <Route path="/contact/de" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
